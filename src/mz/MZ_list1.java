4fd21608ce4374e12c8c8510dfe26142399ba7d0
박히람 <121841669+deswaq1220@users.noreply.github.com>
2023-02-21 17:27:28 +0900

    경원

--- src/mz/MZ_list1.java
+++ src/mz/MZ_list1.java
@@ -60,6 +60,7 @@ public class MZ_list1 {
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		JPanel panel = new JPanel();
+		panel.setBackground(new Color(255, 255, 255));
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -90,88 +91,108 @@ public class MZ_list1 {
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setToolTipText("");
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"★★★★★", "★★★★", "★★★", "★★" ,"★"}));
-		comboBox.setSelectedIndex(5);
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"★★★★★", "★★★★", "★★★","★★","★"}));
+		comboBox.setSelectedIndex(4);
 		
-		JLabel lblNewLabel_3 = new JLabel("영업시간 : 12:00 ~ 20:30");
+		JLabel lblNewLabel_3 = new JLabel("영업시간");
+		lblNewLabel_3.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 26));
 		
 		JLabel lblNewLabel_4 = new JLabel("리뷰");
+		lblNewLabel_4.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 26));
 		
-		JLabel lblNewLabel_4_1 = new JLabel("갯수");
+		JLabel lblNewLabel_4_1 = new JLabel("999+");
+		lblNewLabel_4_1.setBackground(new Color(39, 39, 39));
+		lblNewLabel_4_1.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 16));
 		
 		JLabel lblNewLabel_4_2 = new JLabel("조회수");
+		lblNewLabel_4_2.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 26));
+		lblNewLabel_4_2.setBackground(new Color(0, 0, 0));
 		
 		JLabel lblNewLabel_4_1_1 = new JLabel("0000");
+		lblNewLabel_4_1_1.setBackground(new Color(39, 39, 39));
+		lblNewLabel_4_1_1.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 16));
 		
-		JLabel lblNewLabel_5 = new JLabel("주소");
+		JLabel lblNewLabel_5 = new JLabel("경상남도 창원시 의창구 사림동 어쩌고 저쩌고 ");
+		lblNewLabel_5.setForeground(new Color(128, 128, 128));
+		lblNewLabel_5.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
 		
 		JLabel lblNewLabel_6 = new JLabel("별점");
 		lblNewLabel_6.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_6.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 16));
+		lblNewLabel_6.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 20));
+		
+		JLabel lblNewLabel_3_1 = new JLabel("12:00 ~ 20:30");
+		lblNewLabel_3_1.setBackground(new Color(39, 39, 39));
+		lblNewLabel_3_1.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 16));
+		
+		JLabel lblNewLabel_1_1 = new JLabel("이미지");
+		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_1.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 30));
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addComponent(lblNewLabel_1, GroupLayout.DEFAULT_SIZE, 1050, Short.MAX_VALUE)
 				.addGroup(gl_panel.createSequentialGroup()
 					.addContainerGap()
 					.addComponent(lblNewLabel_2, GroupLayout.PREFERRED_SIZE, 255, GroupLayout.PREFERRED_SIZE)
 					.addGap(9)
 					.addComponent(lblNewLabel_6, GroupLayout.PREFERRED_SIZE, 52, GroupLayout.PREFERRED_SIZE)
-					.addGap(3)
-					.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, 185, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(534, Short.MAX_VALUE))
-				.addGroup(gl_panel.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(lblNewLabel_3, GroupLayout.PREFERRED_SIZE, 195, GroupLayout.PREFERRED_SIZE)
-					.addGap(11)
-					.addComponent(lblNewLabel_4, GroupLayout.PREFERRED_SIZE, 85, GroupLayout.PREFERRED_SIZE)
-					.addGap(24)
-					.addComponent(lblNewLabel_4_1, GroupLayout.PREFERRED_SIZE, 193, GroupLayout.PREFERRED_SIZE)
-					.addGap(13)
-					.addComponent(lblNewLabel_4_2, GroupLayout.PREFERRED_SIZE, 85, GroupLayout.PREFERRED_SIZE)
-					.addGap(37)
-					.addComponent(lblNewLabel_4_1_1, GroupLayout.PREFERRED_SIZE, 193, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(202, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, 134, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(583, Short.MAX_VALUE))
 				.addGroup(gl_panel.createSequentialGroup()
 					.addGap(14)
 					.addComponent(lblNewLabel_5, GroupLayout.PREFERRED_SIZE, 548, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap(488, Short.MAX_VALUE))
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING, false)
+						.addComponent(lblNewLabel_1, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addGroup(Alignment.LEADING, gl_panel.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblNewLabel_3, GroupLayout.PREFERRED_SIZE, 95, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(lblNewLabel_3_1, GroupLayout.PREFERRED_SIZE, 115, GroupLayout.PREFERRED_SIZE)
+							.addGap(29)
+							.addComponent(lblNewLabel_4, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(lblNewLabel_4_1, GroupLayout.PREFERRED_SIZE, 74, GroupLayout.PREFERRED_SIZE)
+							.addGap(29)
+							.addComponent(lblNewLabel_4_2, GroupLayout.PREFERRED_SIZE, 85, GroupLayout.PREFERRED_SIZE)))
+					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(lblNewLabel_4_1_1, GroupLayout.PREFERRED_SIZE, 133, GroupLayout.PREFERRED_SIZE)
+							.addContainerGap(392, Short.MAX_VALUE))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(lblNewLabel_1_1, GroupLayout.PREFERRED_SIZE, 518, GroupLayout.PREFERRED_SIZE))))
 		);
 		gl_panel.setVerticalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel.createSequentialGroup()
-					.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 291, GroupLayout.PREFERRED_SIZE)
 					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 291, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
 							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
 								.addGroup(gl_panel.createSequentialGroup()
 									.addPreferredGap(ComponentPlacement.RELATED)
 									.addComponent(lblNewLabel_2, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE))
 								.addGroup(gl_panel.createSequentialGroup()
 									.addGap(37)
-									.addComponent(lblNewLabel_6))
-								.addGroup(gl_panel.createSequentialGroup()
-									.addGap(34)
-									.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
-							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-								.addGroup(gl_panel.createSequentialGroup()
-									.addPreferredGap(ComponentPlacement.UNRELATED)
-									.addComponent(lblNewLabel_3, GroupLayout.PREFERRED_SIZE, 123, GroupLayout.PREFERRED_SIZE))
-								.addGroup(gl_panel.createSequentialGroup()
-									.addGap(10)
-									.addComponent(lblNewLabel_4, GroupLayout.PREFERRED_SIZE, 123, GroupLayout.PREFERRED_SIZE))
-								.addGroup(gl_panel.createSequentialGroup()
-									.addGap(10)
-									.addComponent(lblNewLabel_4_1, GroupLayout.PREFERRED_SIZE, 123, GroupLayout.PREFERRED_SIZE))))
-						.addGroup(gl_panel.createSequentialGroup()
-							.addGap(95)
-							.addComponent(lblNewLabel_4_2, GroupLayout.PREFERRED_SIZE, 123, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_panel.createSequentialGroup()
-							.addGap(117)
-							.addComponent(lblNewLabel_4_1_1, GroupLayout.PREFERRED_SIZE, 81, GroupLayout.PREFERRED_SIZE)))
-					.addGap(24)
+									.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+										.addComponent(lblNewLabel_6)
+										.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))))
+						.addComponent(lblNewLabel_1_1, GroupLayout.PREFERRED_SIZE, 291, GroupLayout.PREFERRED_SIZE))
+					.addGap(21)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel_3, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_4, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_3_1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(lblNewLabel_4_1, GroupLayout.PREFERRED_SIZE, 103, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_4_2, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_4_1_1, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE))
+					.addGap(27)
 					.addComponent(lblNewLabel_5, GroupLayout.PREFERRED_SIZE, 72, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(100, Short.MAX_VALUE))
+					.addContainerGap(102, Short.MAX_VALUE))
 		);
 		panel.setLayout(gl_panel);
 		frame.getContentPane().setLayout(groupLayout);
