7bf0c8dc92ac7dc1023eeda9a8610f4252f231bc
박히람 <121841669+deswaq1220@users.noreply.github.com>
2023-02-21 18:29:16 +0900

    경원

--- src/mz/MZ_list1.java
+++ src/mz/MZ_list1.java
@@ -18,6 +18,8 @@ import javax.swing.JComboBox;
 import javax.swing.JRadioButton;
 import javax.swing.JEditorPane;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JScrollPane;
+import javax.swing.JScrollBar;
 
 public class MZ_list1 {
 
@@ -52,150 +54,92 @@ public class MZ_list1 {
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(new Color(255, 255, 255));
-		frame.setBounds(100, 100, 1101, 999);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JLabel lblNewLabel = new JLabel("K-푸드");
-		lblNewLabel.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 50));
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		JPanel panel = new JPanel();
-		panel.setBackground(new Color(255, 255, 255));
+		panel.setBackground(Color.WHITE);
+		
+		JScrollPane scrollPane = new JScrollPane();
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 1050, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 256, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(25, Short.MAX_VALUE))
+						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 1083, GroupLayout.PREFERRED_SIZE)
+						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 1084, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 202, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 706, GroupLayout.PREFERRED_SIZE)
-					.addGap(82))
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 195, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 761, Short.MAX_VALUE))
 		);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("K- 푸드");
+		lblNewLabel.setForeground(new Color(0, 0, 0));
+		lblNewLabel.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 54));
+		lblNewLabel.setBounds(36, 39, 211, 101);
+		panel.add(lblNewLabel);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.WHITE);
+		scrollPane.setViewportView(panel_1);
+		panel_1.setLayout(null);
 		
 		JLabel lblNewLabel_1 = new JLabel("이미지");
-		lblNewLabel_1.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 30));
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		
-		JLabel lblNewLabel_2 = new JLabel("리코리코");
-		lblNewLabel_2.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 34));
-		lblNewLabel_2.setHorizontalAlignment(SwingConstants.LEFT);
-		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setToolTipText("");
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"★★★★★", "★★★★", "★★★","★★","★"}));
-		comboBox.setSelectedIndex(4);
-		comboBox.setBackground(new Color(255,255,255));
-		
-		JLabel lblNewLabel_3 = new JLabel("영업시간");
-		lblNewLabel_3.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 26));
-		
-		JLabel lblNewLabel_4 = new JLabel("리뷰");
-		lblNewLabel_4.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 26));
-		
-		JLabel lblNewLabel_4_1 = new JLabel("999+");
-		lblNewLabel_4_1.setBackground(new Color(39, 39, 39));
-		lblNewLabel_4_1.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 16));
-		
-		JLabel lblNewLabel_4_2 = new JLabel("조회수");
-		lblNewLabel_4_2.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 26));
-		lblNewLabel_4_2.setBackground(new Color(0, 0, 0));
-		
-		JLabel lblNewLabel_4_1_1 = new JLabel("0000");
-		lblNewLabel_4_1_1.setBackground(new Color(39, 39, 39));
-		lblNewLabel_4_1_1.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 16));
-		
-		JLabel lblNewLabel_5 = new JLabel("경상남도 창원시 의창구 사림동 어쩌고 저쩌고 ");
-		lblNewLabel_5.setForeground(new Color(128, 128, 128));
-		lblNewLabel_5.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
-		
-		JLabel lblNewLabel_6 = new JLabel("별점");
-		lblNewLabel_6.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_6.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 20));
-		
-		JLabel lblNewLabel_3_1 = new JLabel("12:00 ~ 20:30");
-		lblNewLabel_3_1.setBackground(new Color(39, 39, 39));
-		lblNewLabel_3_1.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 16));
+		lblNewLabel_1.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 28));
+		lblNewLabel_1.setBounds(0, 0, 542, 327);
+		panel_1.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_1_1 = new JLabel("이미지");
 		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1_1.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 30));
-		GroupLayout gl_panel = new GroupLayout(panel);
-		gl_panel.setHorizontalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(lblNewLabel_2, GroupLayout.PREFERRED_SIZE, 255, GroupLayout.PREFERRED_SIZE)
-					.addGap(5)
-					.addComponent(lblNewLabel_6, GroupLayout.PREFERRED_SIZE, 52, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, 134, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(587, Short.MAX_VALUE))
-				.addGroup(gl_panel.createSequentialGroup()
-					.addGap(14)
-					.addComponent(lblNewLabel_5, GroupLayout.PREFERRED_SIZE, 548, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(488, Short.MAX_VALUE))
-				.addGroup(gl_panel.createSequentialGroup()
-					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING, false)
-						.addComponent(lblNewLabel_1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addGroup(gl_panel.createSequentialGroup()
-							.addContainerGap()
-							.addComponent(lblNewLabel_3, GroupLayout.PREFERRED_SIZE, 95, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(lblNewLabel_3_1, GroupLayout.PREFERRED_SIZE, 115, GroupLayout.PREFERRED_SIZE)
-							.addGap(29)
-							.addComponent(lblNewLabel_4, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(lblNewLabel_4_1, GroupLayout.PREFERRED_SIZE, 74, GroupLayout.PREFERRED_SIZE)
-							.addGap(29)
-							.addComponent(lblNewLabel_4_2, GroupLayout.PREFERRED_SIZE, 85, GroupLayout.PREFERRED_SIZE)))
-					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
-						.addGroup(gl_panel.createSequentialGroup()
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(lblNewLabel_4_1_1, GroupLayout.PREFERRED_SIZE, 133, GroupLayout.PREFERRED_SIZE)
-							.addContainerGap(392, Short.MAX_VALUE))
-						.addGroup(gl_panel.createSequentialGroup()
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(lblNewLabel_1_1, GroupLayout.PREFERRED_SIZE, 518, GroupLayout.PREFERRED_SIZE))))
-		);
-		gl_panel.setVerticalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel.createSequentialGroup()
-					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panel.createSequentialGroup()
-							.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 291, GroupLayout.PREFERRED_SIZE)
-							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-								.addGroup(gl_panel.createSequentialGroup()
-									.addGap(18)
-									.addComponent(lblNewLabel_2, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE))
-								.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
-									.addPreferredGap(ComponentPlacement.RELATED, 50, Short.MAX_VALUE)
-									.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-										.addComponent(lblNewLabel_6)
-										.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-									.addGap(25))))
-						.addComponent(lblNewLabel_1_1, GroupLayout.PREFERRED_SIZE, 291, GroupLayout.PREFERRED_SIZE))
-					.addGap(9)
-					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel_3, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel_4, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel_3_1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(lblNewLabel_4_1, GroupLayout.PREFERRED_SIZE, 103, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel_4_2, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel_4_1_1, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE))
-					.addGap(27)
-					.addComponent(lblNewLabel_5, GroupLayout.PREFERRED_SIZE, 72, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(99, Short.MAX_VALUE))
-		);
-		panel.setLayout(gl_panel);
+		lblNewLabel_1_1.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 28));
+		lblNewLabel_1_1.setBounds(540, 0, 542, 327);
+		panel_1.add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("리코리코");
+		lblNewLabel_2.setForeground(new Color(0, 0, 0));
+		lblNewLabel_2.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 45));
+		lblNewLabel_2.setBounds(0, 337, 165, 109);
+		panel_1.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("별점");
+		lblNewLabel_2_1.setForeground(new Color(39, 39, 39));
+		lblNewLabel_2_1.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 30));
+		lblNewLabel_2_1.setBounds(202, 337, 78, 109);
+		panel_1.add(lblNewLabel_2_1);
+		
+		JLabel lblNewLabel_2_1_1 = new JLabel("영업시간");
+		lblNewLabel_2_1_1.setForeground(new Color(0, 0, 0));
+		lblNewLabel_2_1_1.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 30));
+		lblNewLabel_2_1_1.setBounds(10, 429, 118, 109);
+		panel_1.add(lblNewLabel_2_1_1);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setForeground(new Color(255, 198, 2));
+		comboBox.setFont(new Font("맑은 고딕", Font.PLAIN, 20));
+		comboBox.setBackground(new Color(255,255,255));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"★★★★★","★★★★","★★★","★★","★"}));
+		comboBox.setSelectedIndex(4);
+		comboBox.setBounds(288, 378, 118, 35);
+		panel_1.add(comboBox);
+		
+		JLabel lblNewLabel_2_1_1_1 = new JLabel("12:00 ~ 8:30");
+		lblNewLabel_2_1_1_1.setForeground(new Color(39, 39, 39));
+		lblNewLabel_2_1_1_1.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 20));
+		lblNewLabel_2_1_1_1.setBounds(140, 429, 118, 109);
+		panel_1.add(lblNewLabel_2_1_1_1);
+		
+		JLabel lblNewLabel_2_1_1_2 = new JLabel("리뷰");
+		lblNewLabel_2_1_1_2.setForeground(Color.BLACK);
+		lblNewLabel_2_1_1_2.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 30));
+		lblNewLabel_2_1_1_2.setBounds(292, 429, 118, 109);
+		panel_1.add(lblNewLabel_2_1_1_2);
 		frame.getContentPane().setLayout(groupLayout);
+		frame.setBounds(100, 100, 1101, 999);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
