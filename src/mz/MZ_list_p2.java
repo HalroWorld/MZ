cfb218c60d9148288a3d0440159dbd41304ef38d
박히람 <121841669+deswaq1220@users.noreply.github.com>
2023-02-22 19:34:10 +0900

    페이지2 별점자리 확장.

--- src/mz/MZ_list_p2.java
+++ src/mz/MZ_list_p2.java
@@ -119,43 +119,34 @@ public class MZ_list_p2 {
 		open_H.setBounds(10, 429, 118, 109);
 		list_G.add(open_H);
 		
-		JComboBox star = new JComboBox();
-		star.setForeground(new Color(255, 198, 2));
-		star.setFont(new Font("맑은 고딕", Font.PLAIN, 20));
-		star.setBackground(new Color(255,255,255));
-		star.setModel(new DefaultComboBoxModel(new String[] {"★★★★★","★★★★","★★★","★★","★"}));
-		star.setSelectedIndex(4);
-		star.setBounds(455, 377, 165, 35);
-		list_G.add(star);
-		
 		JLabel Hour = new JLabel(db.mzList.getMzHours());
 		Hour.setForeground(new Color(39, 39, 39));
 		Hour.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 20));
-		Hour.setBounds(140, 429, 256, 109);
+		Hour.setBounds(140, 429, 403, 109);
 		list_G.add(Hour);
 		
 		JLabel reveiw = new JLabel("리뷰");
 		reveiw.setForeground(Color.BLACK);
 		reveiw.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 30));
-		reveiw.setBounds(434, 428, 78, 109);
+		reveiw.setBounds(592, 426, 78, 109);
 		list_G.add(reveiw);
 		
 		JLabel reveiw_score = new JLabel(Integer.toString(db.mzList.getMzHit()));
 		reveiw_score.setForeground(new Color(39, 39, 39));
 		reveiw_score.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 20));
-		reveiw_score.setBounds(510, 429, 118, 109);
+		reveiw_score.setBounds(668, 427, 118, 109);
 		list_G.add(reveiw_score);
 		
 		JLabel veiws = new JLabel("조회수");
 		veiws.setForeground(Color.BLACK);
 		veiws.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 30));
-		veiws.setBounds(640, 428, 78, 109);
+		veiws.setBounds(798, 426, 78, 109);
 		list_G.add(veiws);
 		
 		JLabel views_score = new JLabel(Integer.toString(db.mzList.getMzHit()));
 		views_score.setForeground(new Color(39, 39, 39));
 		views_score.setFont(new Font("배달의민족 한나체 Pro", Font.PLAIN, 20));
-		views_score.setBounds(736, 428, 118, 109);
+		views_score.setBounds(894, 426, 118, 109);
 		list_G.add(views_score);
 		
 		JButton btn_storeName = new JButton(db.mzList.getMzTitle());
@@ -205,11 +196,15 @@ public class MZ_list_p2 {
 		lblNewLabel.setBounds(12, 548, 71, 52);
 		list_G.add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel(db.mzList.getMzAddr());
-		lblNewLabel_1.setForeground(new Color(70, 70, 70));
-		lblNewLabel_1.setFont(new Font("맑은 고딕", Font.BOLD, 20));
-		lblNewLabel_1.setBounds(83, 548, 701, 52);
-		list_G.add(lblNewLabel_1);
+		JLabel addr = new JLabel(db.mzList.getMzAddr());
+		addr.setForeground(new Color(70, 70, 70));
+		addr.setFont(new Font("맑은 고딕", Font.BOLD, 20));
+		addr.setBounds(83, 548, 861, 52);
+		list_G.add(addr);
+		
+		JLabel star = new JLabel("여기 별점자리 진짜 왜자꾸 없어지노");
+		star.setBounds(434, 371, 344, 47);
+		list_G.add(star);
 		frame2.getContentPane().setLayout(groupLayout);
 	}
 }
